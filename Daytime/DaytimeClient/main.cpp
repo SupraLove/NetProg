548551329f6e9b70e9fb2012998993cf90d56344
Ramil ♥ <114113891+SupraLove@users.noreply.github.com>
2023-09-26 20:54:17 +0300

    Update main.cpp

--- Daytime/DaytimeClient/main.cpp
+++ Daytime/DaytimeClient/main.cpp
@@ -7,51 +7,36 @@ using namespace std;
 //установить соединение с сервером, выполнить информационный обмен, закрыть сокет
 //формируем структуру
 int main() {
-struct sockaddr_in {
- short sin_family;//для указания протокола ipv4/6
- unsigned short sin_port;//порт
- struct in_addr sin_addr;//адрес ip
- char sin_zero[8];
-};
-struct in_addr {
- unsigned long s_addr;
-};
-//Создание сокета IPv4
-int s = socket(AF_INET, SOCK_DGRAM, 0); // and ipv4 UDP(быстрая передача данных) and 
-if(s == -1) {
-    cout << "Socket error\n";
-    return 1;
-}
+    int s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s == -1) {
+        cout << "Socket error\n";
+        return 1;
+    }
 
- //Подготовка структуры с адресом клиента
-sockaddr_in * self_addr = new (sockaddr_in);
-self_addr->sin_family = AF_INET; // интернет протокол IPv4
-self_addr->sin_port = htons(44214); // Порт
-self_addr->sin_addr.s_addr = inet_addr("127.0.0.1"); //Адрес сети
+    sockaddr_in self_addr;
+    self_addr.sin_family = AF_INET;
+    self_addr.sin_port = htons(44214);
+    self_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
- //Подготовка структуры с адресом сервера
-sockaddr_in * srv_addr = new (sockaddr_in);
-srv_addr->sin_family = AF_INET; // интернет протокол IPv4
-srv_addr->sin_port = htons(44214); //Порт
-srv_addr->sin_addr.s_addr = inet_addr("127.0.0.1"); //Адрес сети
+    sockaddr_in srv_addr;
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_port = htons(44214);
+    srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-//Установка соединения с сервером
-int rc = connect(s,(const sockaddr*)srv_addr, sizeof(sockaddr_in));
-if (rc == -1) { //Проверка
-    cout << "Error: failed connect to server.\n";
-    return 1;
-}
-else {
-    //Выполнение информационного обмена
-    //Передача данных
-    cout << "We are connect to server!\n";
-    char connection_msg[50] = "Hello, it's client!";
-    send(s, connection_msg, sizeof(connection_msg), 0);
-    char daytime[50];
-    recv(s, daytime, sizeof(daytime), 0); //Принятие ответа
-    cout << "Daytime from server: " << daytime << endl;
-    cout << "Session close!" << endl;
-    close(s);
-    return 0;
-}
+    int rc = connect(s, (const sockaddr)&srv_addr, sizeof(sockaddr_in));
+    if (rc == -1) {
+        cout << "Error: failed connect to server.\n";
+        return 1;
+    }
+    else {
+        cout << "We are connected to the server!\n";
+        char connection_msg[50] = "Hello, it's client!";
+        send(s, connection_msg, sizeof(connection_msg), 0);
+        char daytime[50];
+        recv(s, daytime, sizeof(daytime), 0);
+        cout << "Daytime from server: " << daytime << endl;
+        cout << "Session closed!" << endl;
+        close(s);
+        return 0;
+    }
 }
